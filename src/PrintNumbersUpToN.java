e030d9aef6ead43d742a0b497567f24a150162e6
v17365409-wq <v17365409@gmail.com>
2026-08-29 00:10:03 +0530

    Added print numbers from 1 to N

---
+++ src/PrintNumbersUpToN.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintNumbersUpToN {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
+}
