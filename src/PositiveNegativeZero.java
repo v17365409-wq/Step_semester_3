8f193b2926cffa44cbbc0c071dce2871dd7258dc
v17365409-wq <v17365409@gmail.com>
2026-08-29 00:06:48 +0530

    Added positive negative zero classifier

---
+++ src/PositiveNegativeZero.java
@@ -0,0 +1,15 @@
+public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+
+        int number = 15;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
